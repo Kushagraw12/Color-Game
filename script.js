983b37a635ae0eb21d2ec5552ca68a535e00b962
Kushagra <kush12092001@gmail.com>
2020-05-16 18:58:59 +0530

    Init

---
+++ script.js
@@ -0,0 +1,10 @@
+var numsquares = 6;
+var colors = [];
+var pickedColor;
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = documnet.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
+
