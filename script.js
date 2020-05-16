821faa38580d8336228865fc4ac61f330e02ce40
Kushagra <kush12092001@gmail.com>
2020-05-16 19:21:24 +0530

    Updated functions

--- script.js
+++ script.js
@@ -8,3 +8,50 @@ var h1 = documnet.querySelector("h1");
 var resetButton = document.querySelector("#reset");
 var modeButtons = document.querySelectorAll(".mode");
 
+
+init();
+
+function init(){
+    setupModeButtons();
+    setupSquares();
+    reset();
+}
+
+function setupModeButtons(){
+    for (var i=0; i<modeButtons.length; i++){
+        modeButtons[i].addEventListener("click", function(){
+            modeButtons[0].classList.remove("selected");
+            modeButtons[1].classList.remove('selected');
+            this.classList.add("selected");
+            this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+            reset();
+        });
+    }
+}
+
+function setupSquares(){
+
+}
+
+function pickColor(){
+    var random = Math.floor(Math.random() * color.length);
+    return colors[random];
+}
+
+
+function generateRandomColors(num){
+    var arr = [];
+    for (var i=0; i<num; i++){
+        arr.push(randomColor());
+    }  
+    
+    return arr;
+}
+
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
